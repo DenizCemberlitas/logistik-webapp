1e5154e6b3eed74b9097e4256b5b260e32f43a46
Deniz Cemberlitas <150486385+DenizCemberlitas@users.noreply.github.com>
2026-06-01 19:16:33 +0200

    Added: server.js routing

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const produkteRoutes = require("./routes/produkte.js");
+const buchungenRoutes = require("./routes/buchungen.js");
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/produkte", produkteRoutes);
+
+app.use("/api/buchungen", buchungenRoutes);
+
+app.listen(3000, () => console.log("Server läuft auf Port 3000"));
