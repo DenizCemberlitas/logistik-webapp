e4526defe9d0766a11687afe0049162a00fa6f98
Deniz Cemberlitas <150486385+DenizCemberlitas@users.noreply.github.com>
2026-06-03 23:49:39 +0200

    Changed: List style to Tabel, no css yet

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 const API_URL = "http://localhost:3000/api/produkte"
-const listEl = document.getElementById("ProdukteList");
+const listEl = document.getElementById("ProdukteBody");
 const form = document.getElementById("produkteForm");
 
 async function loadProdukte(){
@@ -15,9 +15,52 @@ async function loadProdukte(){
         listEl.innerHTML = "";
 
         for (const it of result){
-            const div = document.createElement("div");
-            div.textContent = it.name + "-" + it.kategorie + "-" + it.einheit + "-" + it.bestand + "-" + it.mindestbestand + "-" + it.lagerort;
-            listEl.appendChild(div);
+            //const div = document.createElement("div");
+            //div.textContent = it.name + "|" + it.kategorie + "|" + it.einheit + "|" + it.bestand + "|" + it.mindestbestand + "|" + it.lagerort;
+            const tr = document.createElement("tr");
+            
+            const tdName = document.createElement("td");
+            tdName.textContent = it.name;
+            tr.appendChild(tdName);
+
+            const tdKategorie = document.createElement("td");
+            tdKategorie.textContent = it.kategorie;
+            tr.appendChild(tdKategorie);
+
+            const tdEinheit = document.createElement("td");
+            tdEinheit.textContent = it.einheit;
+            tr.appendChild(tdEinheit);
+
+            const tdBestand = document.createElement("td");
+            tdBestand.textContent = it.bestand;
+            tr.appendChild(tdBestand);
+
+            const tdMindestbestand = document.createElement("td");
+            tdMindestbestand.textContent = it.mindestbestand;
+            tr.appendChild(tdMindestbestand);
+
+            const tdLagerort = document.createElement("td");
+            tdLagerort.textContent = it.lagerort;
+            tr.appendChild(tdLagerort);
+
+            //Delete 
+            const deleteButton = document.createElement("Button")
+            deleteButton.textContent = "x"
+            deleteButton.addEventListener("click", async (e) => {
+                const deleteProdukt = await fetch(API_URL + "/" + it.id, {
+                    method: "DELETE"
+                });
+                loadProdukte();
+            });
+            
+            //Mindestbestand Warning
+            if (it.bestand < it.mindestbestand){
+                tr.classList.add("warnung");
+            }
+            const tdDelete = document.createElement("td");
+            tdDelete.appendChild(deleteButton);
+            tr.appendChild(tdDelete);
+            listEl.appendChild(tr);
         }
     } catch(error){
         console.error(error.message);
@@ -33,13 +76,13 @@ form?.addEventListener("submit", async (e) => {
     const mindestbestand = document.getElementById("mindestbestandProdukteInput").value;
     const lagerort = document.getElementById("lagerortProdukteInput").value;
     
-    const response = await fetch("http://localhost:3000/api/produkte", {
+    const response = await fetch(API_URL, {
         headers: {
             "Content-Type": "application/json"
         },
         method: "POST",
         body: JSON.stringify({name: name, kategorie: kategorie, einheit: einheit, bestand: bestand, mindestbestand: mindestbestand, lagerort: lagerort})
-    })
+    });
     loadProdukte();
 });
 loadProdukte();
